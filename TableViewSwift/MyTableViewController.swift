de87325638ad86fcc20843459270426391bad4ff
prehooo <kamilnatonek@gmail.com>
2019-09-15 20:50:49 +0200

    Dodanie addButton i checkmarks

--- TableViewSwift/MyTableViewController.swift
+++ TableViewSwift/MyTableViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class MyTableViewController: UITableViewController {
 
-    let itemArray = ["Data1","Data2","Data3","Data4","Data5","Data6","Data7","Data8","Data9","Data10","Data11","Data12"]
+    var itemArray = ["Data1","Data2","Data3","Data4","Data5","Data6","Data7","Data8","Data9","Data10","Data11","Data12"]
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -28,7 +28,39 @@ class MyTableViewController: UITableViewController {
         cell.textLabel?.text = itemArray[indexPath.row]
         return cell
     }
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+       
+    }
 
+    @IBAction func addButton(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "Dodaj item", message: "", preferredStyle: .alert)
+        var temp = UITextField()
+        let action = UIAlertAction(title: "Dodaj", style: .default) { (action) in
+            //coo sie stanie po kliknieciu add button w alercie
+            print("done")
+            print(temp)
+            self.itemArray.append(temp.text!)
+            self.tableView.reloadData() //Ważne!
+            
+            
+        }
+        // dodanie texfield do alertu
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Tutaj wpisz nazwe"
+            temp = alertTextField
+            
+            print("eloelo320")
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    
+    
 
 }
-
+}
